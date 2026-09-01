60f09611a1717af322a23e7e14f163122576386f
Edward Kemp <edwardlanekemp@gmail.com>
2026-09-01 21:39:41 +0100

    refactor: compose app from modular pages

--- scripts/prerender-entry.tsx
+++ scripts/prerender-entry.tsx
@@ -1,6 +1,6 @@
 import { generateHydrationScript, renderToString } from "solid-js/web";
 import App from "../src/App";
-import { allSeoRoutes, seoForPath } from "../src/seoContent";
+import { allSeoRoutes, seoForPath } from "../src/content/seo";
 
 export function renderApp(path = "/") {
   return renderToString(() => <App initialPath={path} />);
